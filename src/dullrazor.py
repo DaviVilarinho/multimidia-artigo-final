14a7dd17baa66f5cb1c1b22fbf87591d056c8747
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-01-03 15:51:05 -0300

    dull razor implementing

---
+++ src/dullrazor.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+"""
+Created on Tue Feb 18 11:42:26 2020
+@author: Javier Velasquez P.
+GET IMAGE BY: img=cv2.imread(path,cv2.IMREAD_COLOR)
+"""
+def dull_razor_on_cv2_img(img):
+    #Gray scale
+    grayScale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY )
+    #Black hat filter
+    kernel = cv2.getStructuringElement(1,(9,9)) 
+    blackhat = cv2.morphologyEx(grayScale, cv2.MORPH_BLACKHAT, kernel)
+    #Gaussian filter
+    bhg= cv2.GaussianBlur(blackhat,(3,3),cv2.BORDER_DEFAULT)
+    #Binary thresholding (MASK)
+    ret,mask = cv2.threshold(bhg,10,255,cv2.THRESH_BINARY)
+    #Replace pixels of the mask
+    dst = cv2.inpaint(img,mask,6,cv2.INPAINT_TELEA)   
+    return dst
+
+cv2.destroyAllWindows()
