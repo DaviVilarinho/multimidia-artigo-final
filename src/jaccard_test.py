7ba7d27dc588d3f0e4f91454cf9e53195a60b228
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-01-17 23:09:54 -0300

    chan_vese method 70%+

--- src/jaccard_test.py
+++ src/jaccard_test.py
@@ -4,19 +4,33 @@ import dullrazor
 import superpixel
 from skimage import io
 from os import listdir
+from skimage.segmentation import chan_vese
+import cv2
 
 DATASET_PATH="/home/dv/files/2022-09_multimedia/datasets/ISBI2016_ISIC_Part1_Test_Data"
 GROUNDTRUTH_PATH="/home/dv/files/2022-09_multimedia/datasets/ISBI2016_ISIC_Part1_Test_GroundTruth"
 images = listdir(DATASET_PATH)
 images = ["ISIC_0000534.jpg"]
 
+
+def get_otsu_thresholded(superpixelized):
+    _, thresholded = cv2.threshold(
+        superpixelized, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return thresholded
+
+
+def get_chan_vese(superpixilized):
+    return chan_vese(superpixilized, max_num_iter=60)
+
 for image in images:
     pic_from_disk = io.imread(f'{DATASET_PATH}/{image}')
     groundtruth = io.imread(f'{GROUNDTRUTH_PATH}/{image.split(".")[0]}_Segmentation.png')
     hair_removed = dullrazor.dull_razor_on_cv2_img(pic_from_disk)
-    for superpixel_qtdd in range(400, 500, 50):
-        superpixelized = superpixel.superpixelize_img(hair_removed / 255, superpixel_qtdd)
-        otsu_proposed = superpixel.otsu_image_superpixel(superpixelized)
+    for superpixel_qtdd in range(400, 700, 50):
+        superpixelized = superpixel.superpixelize_img(
+            hair_removed / 255, superpixel_qtdd)
 
-        jscore = jaccard_score(groundtruth, otsu_proposed, average='micro')
-        print(jscore)
+        print(
+            f'OTSU JAC: \t\t{jaccard_score(groundtruth, get_otsu_thresholded(superpixelized), average="micro")}')
+        print(
+            f'CHAN VESE JAC: \t\t{jaccard_score(groundtruth, get_chan_vese(superpixelized) * 255, average="micro")}')
--- src/superpixel.py
+++ src/superpixel.py
@@ -1,6 +1,5 @@
 from skimage.segmentation import slic
 from skimage import color
-import cv2
 
 def superpixelize_img(image, superpixels_amount):
     segments = slic(image, n_segments = superpixels_amount, start_label=1, compactness=30)
@@ -8,6 +7,3 @@ def superpixelize_img(image, superpixels_amount):
     return (color.rgb2hsv(superpixilized)[:,:,2] * 255).astype('uint8')
 
 
-def otsu_image_superpixel(superpixelized):
-    _,thresholded = cv2.threshold(superpixelized,0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    return thresholded
