547b8f6286dfcf1cf14c53e4dadc4e6fa7962734
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-01-21 18:06:12 -0300

    inicio da minha análise

---
+++ analise_davi.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+import pprint
+
+jaccard_results = pd.read_csv("2023-01-20_jaccard-results.csv")
+jaccard_results.columns = jaccard_results.columns.str.replace(" ", "_")
+
+print(jaccard_results.describe())
+
+# análise das imagens
+print("Análise de média das imagens")
+jaccard_by_image = jaccard_results.groupby(by="Image")
+
+media_por_imagens = jaccard_by_image.mean(numeric_only=True)
+print(media_por_imagens)
+media_por_imagens.plot.bar(
+    y="JAC_Product", title="Média dos resultados das partições Por Imagem")
+# o otsu outperforma o chan-vese na maioria dos casos
+# O produto em geral não foi alto, mas ISIC_0000371.jpg e ISIC_0010058.jpg foram bons pra ambos
+# independentemente, isso indica que em si a necessidade da metodologia indicada no artigo
+print("Análise de desvio padrão das imagens")
+plt.show()
+desvio_padrao_por_imagens = jaccard_by_image.std(numeric_only=True)
+print(desvio_padrao_por_imagens)
+desvio_padrao_por_imagens.plot.bar(y="JAC_Product",
+                                   title="Desvio padrão dos resultados das partições Por Imagem")
+plt.show()
+# o chan-vese apresenta resultados mais precisos, embora tenham outliers
+# o Otsu, por outro lado, não
+# o produto em geral apresenta baixa variabilidade nas imagens
+
+jaccard_by_superpixel = jaccard_results.groupby(
+    by="Superpixel_amount", sort=True)
+print("Analisando a média dos superpixeis")
+media_por_superpixel = jaccard_by_superpixel.mean(numeric_only=True)
+print(media_por_superpixel)
+media_por_superpixel.plot(title="Média dos Resultados por Superpixel")
+plt.show()
+# o otsu apresentou resultados mais consistentes com os superpixeis,
+# mas 400, 450 e 1200 destacam-se por ter as maiores médias e serem os menores valores.
+# quanto ao chan-vese, apresentou bem mais inconsistência com até ~25% de variação com o máximo valor...
+# O maior em produto foi o 450, 550.
+# PORTANTO, da análise de média destaca-se o 450
+print("Analisando o desvio padrão dos superpixeis")
+desvio_padrao_por_superpixel = jaccard_by_superpixel.std(numeric_only=True)
+print(desvio_padrao_por_superpixel)
+desvio_padrao_por_superpixel.plot(
+    title="Desvio Padrão dos Resultados Por Superpixel")
+plt.show()
+# a partir de 1000 existe um plateau de desvio padrão
+# mas que já é alcançado por volta do 450
+
+
+"""
+model = smf.mixedlm("JAC_Product ~ Superpixel_amount",
+                    data=jaccard_results, groups=jaccard_results["Image"])
+
+print(model.fit().summary())
+"""
