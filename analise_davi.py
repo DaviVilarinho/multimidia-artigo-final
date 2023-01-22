4c6a1e86ae4ab14d952cb2aae03930a395182001
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-01-22 14:58:30 -0300

    minha analise atualizada

--- analise_davi.py
+++ analise_davi.py
@@ -5,7 +5,7 @@ import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
 import pprint
 
-jaccard_results = pd.read_csv("2023-01-20_jaccard-results.csv")
+jaccard_results = pd.read_csv("2023-01-22_output/jaccard-results.csv")
 jaccard_results.columns = jaccard_results.columns.str.replace(" ", "_")
 
 print(jaccard_results.describe())
@@ -21,6 +21,9 @@ media_por_imagens.plot.bar(
 # o otsu outperforma o chan-vese na maioria dos casos
 # O produto em geral não foi alto, mas ISIC_0000371.jpg e ISIC_0010058.jpg foram bons pra ambos
 # independentemente, isso indica que em si a necessidade da metodologia indicada no artigo
+#
+# Existem imagens muito difíceis... A média não é evidente,
+# O Chan Vese consegue melhores resultados, enquanto o Otsu os mantém satisfatórios
 print("Análise de desvio padrão das imagens")
 plt.show()
 desvio_padrao_por_imagens = jaccard_by_image.std(numeric_only=True)
@@ -31,6 +34,9 @@ plt.show()
 # o chan-vese apresenta resultados mais precisos, embora tenham outliers
 # o Otsu, por outro lado, não
 # o produto em geral apresenta baixa variabilidade nas imagens
+#
+# Em queral o desvio padrão mantém à níveis satisfatórios e baixos,
+# mas o otsu varia mais
 
 jaccard_by_superpixel = jaccard_results.groupby(
     by="Superpixel_amount", sort=True)
@@ -44,6 +50,9 @@ plt.show()
 # quanto ao chan-vese, apresentou bem mais inconsistência com até ~25% de variação com o máximo valor...
 # O maior em produto foi o 450, 550.
 # PORTANTO, da análise de média destaca-se o 450
+#
+# Quantidades pequenas não ironicamente tem resultados melhores
+# ~450 e 500 novamente destaca-se
 print("Analisando o desvio padrão dos superpixeis")
 desvio_padrao_por_superpixel = jaccard_by_superpixel.std(numeric_only=True)
 print(desvio_padrao_por_superpixel)
@@ -52,11 +61,5 @@ desvio_padrao_por_superpixel.plot(
 plt.show()
 # a partir de 1000 existe um plateau de desvio padrão
 # mas o valor já é alcançado por volta do 450
-
-
-"""
-model = smf.mixedlm("JAC_Product ~ Superpixel_amount",
-                    data=jaccard_results, groups=jaccard_results["Image"])
-
-print(model.fit().summary())
-"""
+#
+# Consistências: 750-1100 e 1250 - 1550
