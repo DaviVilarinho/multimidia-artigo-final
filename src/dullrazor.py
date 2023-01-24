06c84b5d40d1b8edf222bee50b4bc9f645251a6e
Mateus Carmo de Oliveira <mateus_oliveira31@outlook.com>
2023-01-24 17:59:34 -0300

    Comentários

--- src/dullrazor.py
+++ src/dullrazor.py
@@ -2,21 +2,21 @@
 
 import cv2
 """
-Created on Tue Feb 18 11:42:26 2020
 @author: Javier Velasquez P.
-GET IMAGE BY: img=cv2.imread(path,cv2.IMREAD_COLOR)
+Algoritmo DullRazor.
+Algumas modificações foram feitas, descritas no artigo
 """
 def dull_razor_on_cv2_img(img):
-    #Gray scale
+    #Escala de cinza
     grayScale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY )
     #Black hat filter
     kernel = cv2.getStructuringElement(1,(20,20)) 
     blackhat = cv2.morphologyEx(grayScale, cv2.MORPH_BLACKHAT, kernel)
-    #Gaussian filter
+    #Filtro Gaussiano
     bhg= cv2.GaussianBlur(blackhat,(9,9),cv2.BORDER_DEFAULT)
-    #Binary thresholding (MASK)
+    #Thresholding Binário (transforma em máscara)
     ret,mask = cv2.threshold(bhg,10,255,cv2.THRESH_BINARY)
-    #Replace pixels of the mask
+    #Troca os pixeis na imagem original em relação à máscara.
     dst = cv2.inpaint(img,mask,6,cv2.INPAINT_TELEA)
 
     return dst
--- src/jaccard_test.py
+++ src/jaccard_test.py
@@ -23,45 +23,66 @@ def get_chan_vese(superpixilized):
 
 
 def main():
+    # Lê o folder com o dataset
     images = listdir(DATASET_PATH)
+    # Sorteia a leitura (pode vir fora de ordem)
     images.sort()
-    images = images[0:16]
+    # images = images[0:16]
 
     with open('jaccard-results.csv', 'w') as csvfile:
+        # Inicia a escrita dos resultados em um arquivo .csv
         results_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
 
+        # Escreve cabeçalho
         results_writer.writerow(
             ["Image", "Superpixel amount", "Otsu JAC", "Chan-vese JAC", "JAC Product"])
 
+        # Para cada imagem...
         for image in images:
             try:
                 print(f'Analyzing {image}')
+
+                # Carrega imagem e sua ground truth equivalente
                 pic_from_disk = io.imread(f'{DATASET_PATH}/{image}')
                 groundtruth = io.imread(
                     f'{GROUNDTRUTH_PATH}/{image.split(".")[0]}_Segmentation.png')
+
+                # Aplica o DullRazor na imagem (implementado em dullrazor.py)
                 hair_removed = dullrazor.dull_razor_on_cv2_img(pic_from_disk)
                 cv2.imwrite(f'output/{image}_hair-removed.png', hair_removed)
+
                 for superpixel_qty in range(200, 1600, 50):
                     print(
                         f'Analyzing {image} for {superpixel_qty} superpixels')
+
+                    # Aplica uma superpixalização na imagem com uma quantidade específica.
+                    # Divide-se por 255 pois a função exige imagens com valores de cor normalizadas.
                     superpixelized = superpixel.superpixelize_img(
                         hair_removed / 255, superpixel_qty)
 
+                    # Aplica-se Otsu e Chan-Vese
                     otsu = get_otsu_thresholded(superpixelized)
                     chan_v = get_chan_vese(superpixelized)
+
                     cv2.imwrite(
                         f'output/{image}-{superpixel_qty}-otsu.png', otsu)
                     cv2.imwrite(
                         f'output/{image}-{superpixel_qty}-chan.png', chan_v)
-                    # micro because we want fp sp
+
+                    # Calcula-se a Score de Jaccard para os resultados de Otsu e Chan-Vese
+                    # "micro" inclui falsos positivos no cálculo
                     otsu_score = jaccard_score(
                         groundtruth, otsu, average="micro")
                     chan_vese_score = jaccard_score(
                         groundtruth, chan_v, average="micro")
+
                     print(
                         f'Writing {image} results for {superpixel_qty} superpixels')
+
+                    # Escreve resultados
                     results_writer.writerow(
                         [image, superpixel_qty, otsu_score, chan_vese_score, otsu_score * chan_vese_score])
+
             except KeyboardInterrupt:
                 exit()
             except Exception:
